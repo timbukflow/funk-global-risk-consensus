a300d1ce57cbc8beff8f69d7987b8fb76c7f3ee8
Ivo Schwizer <ivoschwizer@gmail.com>
2023-05-08 12:51:07 +0200

    test

--- extreme-wetterereignisse.js
+++ extreme-wetterereignisse.js
@@ -1,15 +1,24 @@
 var data = {
   labels: ["2017", "2018", "2019", "2020", "2021", "2022", "2023"],
   datasets: [{
-    label: "Meine Daten",
+    label: "entwicklung",
     data: [0, 16, 15, 18, 16, 14, 15],
-    borderColor: "rgb(255, 99, 132)",
-    backgroundColor: "rgba(255, 99, 132, 0.2)"
+    borderColor: "#005092",
+    borderWidth: 2,
+    backgroundColor: "#005092",
+   
   }]
 };
 
 // Chart-Optionen definieren
 var options = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 1 // Legendenquadrat auf 1px festlegen
+    },
+    textAlign: 'left' // Textausrichtung der Legendenbeschriftungen auf die linke Seite setzen
+  },
   scales: {
     yAxes: [{
       ticks: {
