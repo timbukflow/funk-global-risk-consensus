5cabe074917bb3c5ba9715f14e14ad7287e8feae
Ivo Schwizer <ivoschwizer@gmail.com>
2023-07-06 16:09:35 +0200

    upd

--- googleanalytics.php
+++ googleanalytics.php
@@ -1,48 +1,60 @@
-  <div id="cookiebanner">
-    <p>Cookies verbessern dein <span class="no-break">Weberlebnis.</span></p>
-    <button id="acceptBtn" onclick="accept()">Okay</button>
-    <button id="declineBtn" onclick="decline()">Nein, danke</button>
-  </div>
+<div id="cookiebanner">
+  <p>Wir verwenden Cookies, um Ihnen das beste Online-Erlebnis zu bieten. Mit Ihrer Zustimmung akzeptieren Sie die Verwendung von Cookies.</p>
+  <button id="acceptBtn">Okay</button>
+  <button id="declineBtn">Nein, danke</button>
+</div>
 
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-  <script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+<script>
     window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
+    function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
 
-    var trackingID = 'G-XXXXXXXXXX';
-    var disableString = 'ga-disable-' + trackingID;
+    const trackingID = 'G-XXXXXXXXXX';
+    const disableString = 'ga-disable-' + trackingID;
+    const cookiebanner = document.getElementById('cookiebanner');
+    const acceptBtn = document.getElementById('acceptBtn');
+    const declineBtn = document.getElementById('declineBtn');
+
+    function setCookie(name, value, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    const expires = "expires="+ d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/;SameSite=None;Secure";
+    }
+
+    function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') { c = c.substring(1); }
+        if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); }
+    }
+    return "";
+    }
 
     function accept() {
-        var expirationDate = new Date('July 1, 2099');
-        document.cookie = 'accepted=yes, i accept google analytics; expires=' + expirationDate.toUTCString() + '; path=/; SameSite=None; Secure';
-        document.getElementById('cookiebanner').remove();
-        gtag('config', trackingID, {
-            'anonymize_ip': true,
-            'allow_google_signals': false,
-            'allow_ad_personalization_signals': false
-        });
+    setCookie('accepted', 'yes, I accept google analytics', 1000);
+    cookiebanner.remove();
+    gtag('config', trackingID, { 'anonymize_ip': true, 'allow_google_signals': false, 'allow_ad_personalization_signals': false });
     }
 
     function decline() {
-        var expirationDate = new Date('July 1, 2099');
-        document.cookie = disableString + '=true; expires=' + expirationDate.toUTCString() + '; path=/; SameSite=None; Secure';
-        document.getElementById('cookiebanner').remove();
+    setCookie(disableString, 'true', 1000);
+    cookiebanner.remove();
     }
 
-    function showBanner() {
-        var banner = document.getElementById('cookiebanner');
-        banner.classList.add('visible');
-    }
+    acceptBtn.addEventListener('click', accept);
+    declineBtn.addEventListener('click', decline);
 
-    if (document.cookie.indexOf(disableString + '=true') > -1) {
-        window[disableString] = true;
-        document.getElementById('cookiebanner').remove();
-    } else if (document.cookie.indexOf('accepted=') >= 0) {
-        accept();
+    if (getCookie(disableString) === 'true') {
+    window[disableString] = true;
+    cookiebanner.remove();
+    } else if (getCookie('accepted') === 'yes, I accept google analytics') {
+    accept();
     } else {
-        setTimeout(showBanner, 3500);
+    setTimeout(() => { cookiebanner.classList.add('visible'); }, 3500);
     }
-  </script>
+</script>
