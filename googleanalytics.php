3ec5be57fcf3dbf7ab1371dd6d22bc03b65e40ba
Ivo Schwizer <ivoschwizer@gmail.com>
2023-07-08 10:33:10 +0200

    mo

--- googleanalytics.php
+++ googleanalytics.php
@@ -1,3 +1,55 @@
+
+
+<div id="cookiebanner">
+    <p>Cookies verbessern dein <span class="no-break">Weberlebnis.</span></p>
+    <button id="acceptBtn" onclick="accept()">Okay</button>
+    <button id="declineBtn" onclick="decline()">Nein, danke</button>
+  </div>
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2N5XW0GKCX"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    var trackingID = 'G-2N5XW0GKCX';
+    var disableString = 'ga-disable-' + trackingID;
+
+    function accept() {
+        var expirationDate = new Date('July 1, 2099');
+        document.cookie = 'accepted=yes, i accept google analytics; expires=' + expirationDate.toUTCString() + '; path=/; SameSite=None; Secure';
+        document.getElementById('cookiebanner').remove();
+        gtag('config', trackingID, {
+            'anonymize_ip': true,
+            'allow_google_signals': false,
+            'allow_ad_personalization_signals': false
+        });
+    }
+
+    function decline() {
+        var expirationDate = new Date('July 1, 2099');
+        document.cookie = disableString + '=true; expires=' + expirationDate.toUTCString() + '; path=/; SameSite=None; Secure';
+        document.getElementById('cookiebanner').remove();
+    }
+
+    function showBanner() {
+        var banner = document.getElementById('cookiebanner');
+        banner.classList.add('visible');
+    }
+
+    if (document.cookie.indexOf(disableString + '=true') > -1) {
+        window[disableString] = true;
+        document.getElementById('cookiebanner').remove();
+    } else if (document.cookie.indexOf('accepted=') >= 0) {
+        accept();
+    } else {
+        setTimeout(showBanner, 3500);
+    }
+  </script>
+<!-- 
+
 <div id="cookiebanner" class="bg">
   <p>Wir verwenden Cookies, um Ihnen das beste Online-Erlebnis zu bieten. Mit Ihrer Zustimmung akzeptieren Sie die Verwendung von Cookies.</p>
   <button id="acceptBtn">Okay</button>
@@ -57,4 +109,4 @@
     } else {
     setTimeout(() => { cookiebanner.classList.add('visible'); }, 3500);
     }
-</script>
+</script> -->
