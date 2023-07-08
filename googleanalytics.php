2de2803201edebe313206d976db3c0742e69abc0
Ivo Schwizer <ivoschwizer@gmail.com>
2023-07-08 10:23:04 +0200

    fix

--- googleanalytics.php
+++ googleanalytics.php
@@ -20,7 +20,7 @@
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     const expires = "expires="+ d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/;SameSite=None;Secure";
+    document.cookie = name + "=" + value + ";" + expires + ";path=/;SameSite=Lax;Secure";
     }
 
     function getCookie(cname) {
